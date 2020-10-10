67da066df8705db30022289a58b74492a169719c
tee.nage.ninja <teenageninj@gmail.com>
2020-10-10 13:43:04 +0300

    add new

---
+++ js/title.js
@@ -0,0 +1,7 @@
+function title() {
+    var width = document.documentElement.clientWidth;
+    var height = document.documentElement.clientHeight;
+    document.title = "Shell — " + width + "x" + height;
+}
+title();
+window.addEventListener('resize', title);
