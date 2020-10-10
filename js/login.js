686bea0ac412518fbd80d1ea8bb73f2d4b0856b8
tee.nage.ninja <teenageninj@gmail.com>
2020-10-10 13:25:42 +0300

    add new

---
+++ js/login.js
@@ -0,0 +1,4 @@
+var date = Date();
+document.getElementById("login__date").textContent = date;
+var userAgent = navigator.userAgent;
+document.getElementById("login__user-agent").textContent = userAgent;
